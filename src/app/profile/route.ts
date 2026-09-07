e1e46992b48ce4a74c96ea680546607f882207da
Nisarg-jain <jainnisag98@gmail.com>
2026-09-07 19:01:47 +0530

    "feat: read and set headers in profile route handler"

---
+++ src/app/profile/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders.get("Authorization"));
+
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+
+  return new Response("<h1>Profile API Data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
+  });
+}
