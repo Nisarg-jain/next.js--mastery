51e1bf0b510d9b17f344e57ca044cc404b2552a8
Nisarg-jain <jainnisag98@gmail.com>
2026-09-08 17:21:08 +0530

    "feat: read and set cookies in route handlers"

--- src/app/profile/route.ts
+++ src/app/profile/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
@@ -7,6 +7,13 @@ export async function GET(request: NextRequest) {
 
   const headerList = await headers();
   console.log(headerList.get("Authorization"));
+
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultsPerPage", "20");
+  console.log(cookieStore.get("resultsPerPage"));
 
   return new Response("<h1>Profile API Data</h1>", {
     headers: {
